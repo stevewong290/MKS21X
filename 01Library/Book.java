93b91b98b17d680eb6f5d5808d5b59a095eba8a7
stevewong290 <stevewong290@gmail.com>
2016-12-01 14:16:24 -0500

    Update Book.java

--- 01Library/Book.java
+++ 01Library/Book.java
@@ -1,1 +1,33 @@
-
+public class Book{
+    private String author;
+    private String title; 
+    private String ISBN;
+    public Book(){ } 
+    public Book(String a,String t, String i){
+	author = a;
+	title = t;
+	ISBN = i;
+    }
+    public String getAuthor(){
+	return author;
+    }
+    public String getTitle(){
+	return title;
+    }
+    public String getISBN(){
+	return ISBN;
+    } 
+    public void setAuthor(String s){
+	author = s;
+    }
+    public void setTitle(String s){
+	title = s;
+    } 
+    public void setISBN(String s){
+	ISBN = s;
+    }
+    public String toString(){
+	return title + ", "+author +", "+ISBN;
+    }
+}
+ 
