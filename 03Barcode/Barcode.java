4f01984e0828691537be710195212d58c18c3b61
Steve Wong <swong14@stuy.edu>
2016-12-07 05:37:32 -0500

    Basic Template

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,1 +1,28 @@
+public class Barcode implements Comparable<Barcode>{
+// instance variables
+   private String _zip;
+   private int _checkDigit;
 
+// constructors
+//precondtion: _zip.length() = 5 and zip contains only digits.
+//postcondition: throws a runtime exception zip is not the correct length
+//               or zip contains a non digit
+//               _zip and _checkDigit are initialized.
+  public Barcode(String zip) {}
+
+// postcondition: Creates a copy of a bar code.
+  public Barcode clone(){}
+
+
+// postcondition: computes and returns the check sum for _zip
+  private int checkSum(){}
+
+//postcondition: format zip + check digit + Barcode 
+//ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
+  public String toString(){}
+
+
+// postcondition: compares the zip + checkdigit, in numerical order. 
+  public int compareTo(Barcode other){}
+    
+}
