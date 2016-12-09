69f03400a9aaca338df45b8e2a59fe80d14a99c3
Steve Wong <swong14@stuy.edu>
2016-12-09 05:50:35 -0500

    Updated constructor

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,3 +1,5 @@
+
+
 public class Barcode implements Comparable<Barcode>{
 // instance variables
    private String _zip;
@@ -8,18 +10,36 @@ public class Barcode implements Comparable<Barcode>{
 //postcondition: throws a runtime exception zip is not the correct length
 //               or zip contains a non digit
 //               _zip and _checkDigit are initialized.
-  public Barcode(String zip) {}
+  public Barcode(String zip) {
+      if(zip.length = 5){
 
-// postcondition: Creates a copy of a bar code.
-  public Barcode clone(){}
+      }
+      
+}
 
+  public boolean isNumValid(zip){
+      for (int i = 0) {
+      }
+
+  }
 
 // postcondition: computes and returns the check sum for _zip
-  private int checkSum(){}
+  private int checkSum(){
+      int sum = 0;
+      for (int i = 0; i < _zip.length; i++) {
+	  sum = sum + Integer.parseInt(i);
+	      }
+      return sum;
+
+}
 
 //postcondition: format zip + check digit + Barcode 
 //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
-  public String toString(){}
+  public String toString(){
+      String temp = "";
+      temp = temp + _zip;
+
+}
 
 
 // postcondition: compares the zip + checkdigit, in numerical order. 
