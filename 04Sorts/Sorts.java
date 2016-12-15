cc0a35fb5aa89730286ddf92ca1b5e3fbd569526
Steve Wong <swong14@stuy.edu>
2016-12-15 05:53:44 -0500

    fixing my insertion

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -1,32 +1,57 @@
 public class Sorts{
-  /**EDIT THIS METHOD TO MATCH YOUR NAME 
-  */
-  public static String name(){
-    return "01.Wong.Steve"; 
-  }
+    /**EDIT THIS METHOD TO MATCH YOUR NAME 
+     */
+    public static String name(){
+	return "01.Wong.Steve"; 
+    }
 
-  /**Selection sort of an int array. 
-  *Upon completion, the elements of the array will be in increasing order.
-  *@param data  the elements to be sorted.
-  */
-  public static void selectionSort(int[] data){
-      int indexCurrentLowestElement = 0;
-      for (int i = 0; i < data.length; i++) {
-	  for (int j = 0; j < data.length; j++) {
-	      if (data[j] < data[indexCurrentLowestElement]){
-		  indexCurrentLowestElement = j;
-	      }
-	  }
-	  data[j] = data[i];
-	  data[i] = data[indexCurrentLowestElement];
-      }
-  }
+    /**Selection sort of an int array. 
+     *Upon completion, the elements of the array will be in increasing order.
+     *@param data  the elements to be sorted.
+     */
+    public static void selectionSort(int[] data){
+	int indexCurrentLowestElement = 0;
+	for (int i = 0; i < data.length; i++) {
+	    for (int j = 0; j < data.length; j++) {
+		if (data[j] < data[indexCurrentLowestElement]){
+		    indexCurrentLowestElement = j;
+		}
+	    
+		data[j] = data[i];
+	    }
+	    data[i] = data[indexCurrentLowestElement];
+	}
+    }
     public static void insertionSort(int[] data){
 	int indexCurrentval = 0;
 	for (int i = 1; i < data.length; i++) {
 	    indexCurrentval = i;
 	    for(int j = i; j >= 0; j--) {
-		if (data[indexCurrentval] < data[j-1]) {
+		if (data[indexCurrentval] <= data[j-1]) {
+		    indexCurrentval = j;
+		    break;
+		}
+		data[j] = data[i];
+	    }
+	    
+	    data[i] = data[indexCurrentval];
+	}
+    }
+
+    public static void print(int[] data) {
+	String ans = "";
+	for (int i = 0; i < data.length; i++) {
+	    ans = ans + data[i] + ", ";
+	}
+	System.out.println("[ " + ans + "]");
+    }
+    public static void main(String args) {
+	int[] Test = {5, 4, 3, 2, 1};
+	print Test;
+	insertionSort(Test);
+	print Test;
+    }
+
 		    
 		    
 }
